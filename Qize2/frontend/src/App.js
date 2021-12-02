a74f37ea95f2460125ae2743f220cb992cee7026
IbrahimT1000 <92294345+IbrahimT1000@users.noreply.github.com>
2021-12-02 12:41:22 +0300

    Update App.js

--- Qize2/frontend/src/App.js
+++ Qize2/frontend/src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import axios from "axios";
 import { Route } from 'react-router-dom';
-
 const App = () => {
-	return <div className="App">App component</div>;
+	const [data, setData] = useState([]);
+  const navigation = useNavigate();
+  useEffect(() => {
+    axios({
+      method: "post",
+      url: "/",
+      data: {
+        id: uId,
+      },
+    })
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {});
+  }, []);
+  console.log(data);
+
 };
 
 export default App;
